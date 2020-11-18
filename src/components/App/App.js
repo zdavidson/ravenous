aa8aa44c34be42d294bb80616d4e5fe83e7defd4
Zoe Davidson <46529583+zdavidson@users.noreply.github.com>
2020-11-17 23:34:53 -0500

    react development part 1

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+function App() {
+  return (
+    <div class="App">
+      <h1>ravenous</h1>
+       <SearchBar />
+       <BusinessList />
+    </div>
+  );
+}
+
+export default App;
