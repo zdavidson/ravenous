91d7fe4db968416fa4772aa09df3860f348fff4c
Zoe Davidson <46529583+zdavidson@users.noreply.github.com>
2020-11-21 14:15:46 -0500

    ravenous front-end complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,37 @@
 import React from 'react';
 import './App.css';
 
-import BusinessList from '../BusinessList/BusinessList.js';
-import SearchBar from '../SearchBar/SearchBar.js';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipcode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business, 
-  business, 
-  business, 
-  business, 
-  business, 
-  business
-];
-
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
   render() {
     return (
-      <div class="App">
+      <div className="App">
         <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businesses}/>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
 }
-
 
 export default App;
